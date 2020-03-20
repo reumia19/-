335c2bcec53c0e01c50db2e5201fbbc08dbf2b8f
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-20 20:13:41 +0900

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,12 @@
+$(function(){
+	$.ajax({
+		url: ,
+		dataType: ,
+		success: function(data){
+			$(data).find('book').each(function(){
+				var title = $('title',this).text();
+				
+			})
+		}
+	})
+})
