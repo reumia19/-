ca15bf250e6ddc718890a6ed6aa67acdd0d8c5b8
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-23 14:52:15 +0900

    Update googleMapAPI.js

--- js/googleMapAPI.js
+++ js/googleMapAPI.js
@@ -1,5 +1,10 @@
 const dMap =document.querySelector(".js-map");
-
+const setting ={
+	center:{lat:37.3339337,lng:126.835851},
+	scrollwheel : false,
+	zoom : 8
+}
+const newMap = new google.maps.Map(dMap,setting);
 function initMap() {
 }
 
